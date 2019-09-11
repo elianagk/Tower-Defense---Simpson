f4fdd739a4ac3bbda45fca00803c9d4fd2f6d7fc
lourdes-P <lourdespanzone@gmail.com>
2019-09-11 16:33:37 -0300

    hice el paquete torre

--- Proyecto TDP/src/Mapa/Mapa.java
+++ Proyecto TDP/src/Mapa/Mapa.java
@@ -1,7 +1,15 @@
 package Mapa;
 
+import GameObject.*;
+
 public class Mapa  {
+	static Personaje torres[]= new Torre [5] , enemigos[] = new Enemigo[6];
 	
+	public Mapa () {
+		for (Personaje torre : torres) {
+			
+		}
+	}
 }
 
 //Lista de personajes
