7143a7e04a4f2450a3f8ecb85ee7e08c9e080d0e
MatiasEG <54218253+MatiasEG@users.noreply.github.com>
2019-09-12 17:13:13 -0300

    llistener mouse

---
+++ Proyecto TDP/src/GRAFICA/Mouse.java
@@ -0,0 +1,40 @@
+package GRAFICA;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class Mouse implements MouseListener{
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		int x=e.getX();
+	    int y=e.getY();
+	    
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
