53c12c4495f7950524099e02e8357f8000e56430
MatiasEG <54218253+MatiasEG@users.noreply.github.com>
2019-09-22 19:11:23 -0300

    ...

--- Proyecto TDP/src/Tienda/Botones/Boton.java
+++ Proyecto TDP/src/Tienda/Botones/Boton.java
@@ -6,9 +6,11 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 
 import GameObject.GameObject;
+import Tienda.TiendaLogica;
 
 public class Boton extends JButton {
 	
+	protected TiendaLogica tienda;
 	protected GameObject miobjeto;
 	
 	public Boton() {
@@ -16,7 +18,8 @@ public class Boton extends JButton {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				// TODO Auto-generated method stub
+				//creo el tipo logico
+				//le paso a la tienda logica (el tipo logico)
 				
 			}
 			
