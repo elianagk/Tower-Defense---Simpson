c478f0b1b51b3d9ca1858bd944c5c491a37d8344
eliko <eliko@DESKTOP-B03MU0I>
2019-10-16 20:17:58 -0300

    BOTONES REDONDOS

--- Proyecto TDP/src/Tienda/Botones/Boton.java
+++ Proyecto TDP/src/Tienda/Botones/Boton.java
@@ -2,13 +2,17 @@ package Tienda.Botones;
 
 
 
+import java.awt.Component;
+import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.border.Border;
 
 import GameObject.GameObject;
 import GameObject.Personajes.Torre;
@@ -23,6 +27,8 @@ public class Boton extends JButton {
 		this.tiendaLogica= tiendaLogica;
 		this.setText("");
 		this.setSize(100, 100);
+		this.setBorder(new RoundedBorder(50));
+		this.setContentAreaFilled(false);
 		ImageIcon fot= new ImageIcon(getClass().getClassLoader().getResource(s));
 		Icon i= new ImageIcon(fot.getImage().getScaledInstance(this.getWidth(), this.getHeight(), Image.SCALE_DEFAULT));
 		this.setIcon(i);
@@ -33,6 +39,32 @@ public class Boton extends JButton {
 				tiendaLogica.setEntidad(t.clone());
 			}
 		});	
+	}
+	
+	
+	private static class RoundedBorder implements Border {
+
+	    private int radius;
+
+
+	    RoundedBorder(int radius) {
+	        this.radius = radius;
+	    }
+
+
+	    public Insets getBorderInsets(Component c) {
+	        return new Insets(this.radius+1, this.radius+1, this.radius+2, this.radius);
+	    }
+
+
+	    public boolean isBorderOpaque() {
+	        return true;
+	    }
+
+
+	    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+	        g.drawRoundRect(x, y, width-1, height-1, radius, radius);
+	    }
 	}
 	
 	
