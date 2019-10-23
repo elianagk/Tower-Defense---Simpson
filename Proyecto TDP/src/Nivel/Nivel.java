3903a91a2ae749ebae0d7a5355225895f1ae8d1f
lourdes-P <lourdespanzone@gmail.com>
2019-10-23 16:48:52 -0300

    nivel
    hay que ver si se puede hacer que se empiece una horda una vez que el ultimo de la horda anterior murio o ver si se deja asi (ahora una horda empieza unos segundos despues de que el ultimo de la ronda anterior salio, no importa si murio o no

--- Proyecto TDP/src/Nivel/Nivel.java
+++ Proyecto TDP/src/Nivel/Nivel.java
@@ -6,9 +6,6 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
-
-//import static java.util.concurrent.TimeUnit.*;
-
 import GameObject.GameObject;
 
 import Hordas.Horda;
@@ -20,16 +17,15 @@ public class Nivel {
 	
 	private Juego juego;
 	private MapaLogico mapaLogico;
-
-
+	private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
 	
 	public Nivel(Juego juego, MapaLogico mapaLogico) {
 		this.juego= juego;
 		this.mapaLogico= mapaLogico;
 		hordas= new Horda[3];
-		hordas[0]= new Horda(this, mapaLogico, 10);
-		hordas[1]= new Horda(this, mapaLogico, 15);
-		hordas[2]= new Horda(this, mapaLogico, 20);
+		hordas[0]= new Horda(this, mapaLogico, 1);
+		hordas[1]= new Horda(this, mapaLogico, 2);
+		hordas[2]= new Horda(this, mapaLogico, 3);
 	}
 	
 	public void crearEnemigo(Horda horda) {
@@ -38,7 +34,7 @@ public class Nivel {
 	}
 	
 	public void ejecutarHorda(Horda horda) {
-		final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+//		final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 		
 		final ScheduledFuture<?> crearHorda = 
 	    scheduler.scheduleAtFixedRate(new Runnable() {
@@ -49,42 +45,30 @@ public class Nivel {
 	     
 	    scheduler.schedule(new Runnable() {public void run() { crearHorda.cancel(true); }
 	    }, horda.getCantEnemigos()*5-5, TimeUnit.SECONDS);
-	     
-
-//
-//	    	     final Runnable beeper = new Runnable() {
-//	    	       public void run() { System.out.println("beep"); }
-//	    	     };
-//	    	     final ScheduledFuture<?> beeperHandle =
-//	    	       scheduler.scheduleAtFixedRate(beeper, 10, 10, SECONDS);
-//	    	     scheduler.schedule(new Runnable() {
-//	    	       public void run() { horda.finalizarHorda(); }
-//	    	     }, horda.getCantEnemigos()*5, SECONDS);
-
-	
-//			while(!horda.finalizarHorda()) {
-//				
-//					g= horda.crearEnemigo();
-//					mapaLogico.entidadAAgregar(g, g.getX(), g.getY());
-//					
-//						
-//			}	
-
 	}
 	
 	public void ejecutarHordas() {
-		final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-		final AtomicInteger cont = new AtomicInteger(0);
 
-		final ScheduledFuture<?> crearHordas = 
-	    scheduler.scheduleAtFixedRate(new Runnable() {
-	    	 @Override
-	    	 public void run() { 
-	    		 ejecutarHorda(hordas[cont.getAndIncrement()]);  //llamo al metodo con una posicion del arreglo cuyo contador luego incremento
-	    		 if (cont.get()==3)
-	    			 scheduler.shutdown();		//cuando el contador llega a tres, la repeticion para
-	    	}
-	     }, 5, hordas[cont.get()].getCantEnemigos()*5, TimeUnit.SECONDS);		
+		scheduler.schedule(new Runnable() {public void run() { ejecutarHorda(hordas[0]); }
+	    }, 5, TimeUnit.SECONDS);
+		
+		
+		scheduler.schedule(new Runnable() {public void run() { ejecutarHorda(hordas[1]);	 }
+	    }, hordas[0].getCantEnemigos()*5+15, TimeUnit.SECONDS);
+			
+		
+		scheduler.schedule(new Runnable() {public void run() { ejecutarHorda(hordas[2]); }
+	    }, hordas[0].getCantEnemigos()*5+15+hordas[1].getCantEnemigos()*5+10, TimeUnit.SECONDS);
+		
+		
+//	    scheduler.scheduleAtFixedRate(new Runnable() {
+//	    	 @Override
+//	    	 public void run() { 
+//	    		 ejecutarHorda(hordas[cont.getAndIncrement()]);  //llamo al metodo con una posicion del arreglo cuyo contador luego incremento
+//	    		 if (cont.get()==3)
+//	    			 scheduler.shutdown();		//cuando el contador llega a tres, la repeticion para
+//	    	}
+//	     }, 5, hordas[cont.get()].getCantEnemigos()*5, TimeUnit.SECONDS);		
 	}
 	
 	public void finalizarNivel() {
